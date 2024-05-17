ecea491e53dd589b0dfc047bbf9415e620e04685
Shiva Gurung <shivakgrg@gmail.com>
2024-05-17 09:47:58 +0545

    updated parsePlace func

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function autoScroll(page) {
   await page.evaluate(async () => {
@@ -24,12 +24,26 @@ async function autoScroll(page) {
 }
 
 async function parsePlaces(page) {
+  console.log("check");
+
   let places = [];
-  const elements = await page.$$('.qBF1Pd.fontHeadlineSmall');
+  // const elements = await page.$$(".qBF1Pd.fontHeadlineSmall");
+  // if (elements && elements.length) {
+  //   for (const el of elements) {
+  //     const name = await el.evaluate((span) => span.textContent);
+  //     // places.push({ name });
+  //   }
+  // }
+  const elements = await page.$$(".Nv2PK.THOPZb.CpccDe");
+  console.log(elements);
   if (elements && elements.length) {
     for (const el of elements) {
-      const name = await el.evaluate(span => span.textContent);
-      places.push({ name });
+      const link = $(el).find("a").attr("href");
+      console.log(link);
+      // const browserInner = await puppeteer.launch({ headless: false });
+      // const pageInner = await browserInner.newPage();
+      // await pageInner.setViewport({ width: 1300, height: 2000 });
+      // await pageInner.goto(link);
     }
   }
   return places;
@@ -39,16 +53,17 @@ async function parsePlaces(page) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1300, height: 2000 });
-  await page.goto('https://www.google.com/maps/search/hotels+in+pokhara/@28.1428736,84.1857381,9.55z');
+  await page.goto(
+    "https://www.google.com/maps/search/hotels+in+pokhara/@28.1428736,84.1857381,9.55z"
+  );
 
   // // Initial parse of places
   // let places = await parsePlaces(page);
   // console.log(places);
   let places = [];
 
-  await autoScroll(page)
+  await autoScroll(page);
   setInterval(async () => {
-   
     const newPlaces = await parsePlaces(page);
     if (newPlaces.length > places.length) {
       places = newPlaces;
